572ac24a0bd421497ace4e61e10d17aa3ea33573
osallahstevie <osallahstevie@gmail.com>
2022-11-30 15:25:10 +0300

    Completed functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(satadayActivity = "roller-skate"){
+    return `This Saturday, I want to ${satadayActivity}!`
+}
+
+const mondayWork = function(office = 'go to the office'){
+    return `This Monday, I will ${office}.`;
+    
+}
+
+function wrapAdjective(result = '*'){
+
+    return function(george = 'special'){
+
+        return `You are ${result}${george}${result}!`
+
+    }
+}
+const promptFun = wrapAdjective("!!!");
